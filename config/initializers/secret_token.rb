065ab05733f8715d6321db18315d5186a46cf6b7
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-27 11:26:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TownOMatic::Application.config.secret_token = '5b782147826cd628451040e0f4a1e27dec28c0d0b895c45bff7ca344c425fdd6e3326ddff9e33e2aac1c010541c17c10481ab7a82374a58e0b46ff98eb272fd0'
