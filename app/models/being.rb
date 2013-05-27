d9dfade61cf07e24736739efce00ef87a9105d1a
Keith Avery <keith.marshall.avery@gmail.com>
2013-05-27 18:39:54 -0400

    homepage and being index page

--- app/models/being.rb
+++ app/models/being.rb
@@ -1,8 +1,30 @@
 class Being
   include Mongoid::Document
-  field :name, :type => Array
+  field :names, :type => Array
   field :age, :type => Integer
   field :alive, :type => Boolean
   field :history, :type => Array
   field :gender, :type => String
+
+  ##
+  # Is this being alive?
+  #
+  def alive?
+    !!alive
+  end
+
+  ##
+  # Display the name of the creature
+  #
+  def name
+    names.join(' ')
+  end
+
+  ##
+  # Is this being dead?
+  #
+  def dead?
+    !alive
+  end
+
 end
