30c1ca36d2a973adaafc67370c784c01dc2258ce
Keith Avery <slabgorb@gmail.com>
2014-09-21 18:33:59 -0400

    spouses and step-siblings

--- lib/townomatic/family.rb
+++ lib/townomatic/family.rb
@@ -3,11 +3,14 @@ module Townomatic
 
     class Member
 
-      attr_reader :parents, :children
+      attr_reader :parents, :children, :spouses
+      attr_accessor :name
 
-      def initialize
+      def initialize(name='')
         @children = []
         @parents = []
+        @name = name
+        @spouses = []
       end
 
       def <<(child)
@@ -18,14 +21,35 @@ module Townomatic
         self.parents.empty?
       end
 
-      def parent=(member)
-        @parents << member
+      def add_parent(parent)
+        raise "Parent already added" if @parents.include?(parent)
+        @parents << parent
+      end
+
+      def marry(spouse)
+        raise "Already married" if @spouses.include?(spouse)
+        @spouses << spouse
+      end
+
+      def divorce!(spouse)
+        raise "Not a spouse" unless @spouses.include?(spouse)
+        @spouses.delete spouse
+      end
+
+      def disown!(child)
+        raise "Not a child" unless @children.include?(child)
+        @children.delete child
+      end
+
+      def siblings
+        parents.map{ |parent| parent.children }.flatten.keep_if{ |c| c != self }
       end
 
       def add(child)
         raise "Child already added" if @children.include?(child)
         @children << child
-        child.parent = self
+        child.add_parent self
+        @spouses.each{ |spouse| child.add_parent(spouse) }
       end
     end
 
--- spec/family/member_spec.rb
+++ spec/family/member_spec.rb
@@ -1,26 +1,58 @@
 require 'spec_helper'
 describe Townomatic::Family::Member do
-  before :all do
-    @adam = Townomatic::Family::Member.new
+  before :each do
+    @adam = Townomatic::Family::Member.new('Adam')
+    @child = Townomatic::Family::Member.new('Cain')
+    @eve = Townomatic::Family::Member.new('Eve')
+    @adam.marry @eve
+    @adam << @child
+    @delilah = Townomatic::Family::Member.new('Delilah')
+  end
+
+  it 'knows spouses' do
+    expect(@adam.spouses.include?(@eve)).to be true
+  end
+
+  it 'allows divorces' do
+    @adam.divorce! @eve
+    expect(@adam.spouses.include?(@eve)).to be false
+  end
+
+  it 'disallows divorces to unmarried people' do
+    @adam.divorce! @eve
+    expect { @adam.divorce! @delilah }.to raise_error
   end
 
   it 'adds a child to a parent' do
-    child = Townomatic::Family::Member.new
-    @adam << child
-    expect(@adam.children.include?(child)).to be true
+    expect(@adam.children.include?(@child)).to be true
   end
 
   it 'disallows adding an extant child to a parent' do
-    child = Townomatic::Family::Member.new
-    @adam << child
-    expect { @adam << child }.to raise_error
+    expect { @adam << @child }.to raise_error
   end
 
   it 'knows the parent of a child' do
-    child = Townomatic::Family::Member.new
-    @adam << child
-    expect(child.parents.include?(@adam)).to be true
+    expect(@child.parents.include?(@adam)).to be true
   end
 
+  it 'knows the spousal parent of a child' do
+    expect(@child.parents.include?(@eve)).to be true
+  end
+
+  it 'knows siblings of a child' do
+    child2 = Townomatic::Family::Member.new
+    @adam << child2
+    expect(@child.siblings.include?(child2)).to be true
+  end
+
+  it 'knows siblings do not include the member' do
+    expect(@child.siblings.include?(@child)).to be false
+  end
+
+  it 'knows about half-siblings' do
+    step_sibling = Townomatic::Family::Member.new
+    @eve << step_sibling
+    expect(@child.siblings.include?(step_sibling)).to be true
+  end
 
 end
